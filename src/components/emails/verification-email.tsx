29948ad07b478c302f79956eeb4c638015635558
yash91989201 <yashraj6370@gmail.com>
2024-03-06 10:56:18 +0530

    style: update styling for verification email

--- src/components/emails/verification-email.tsx
+++ src/components/emails/verification-email.tsx
@@ -21,20 +21,20 @@ export default function VerificationEmail({
   return (
     <Html>
       <Tailwind>
-        <Body className="bg-indigo-500">
-          <Container className="mx-auto my-16 flex min-w-[400px] flex-col items-center  rounded-md border bg-white p-6 shadow-md ">
-            <Heading>🔐 Next Auth V5</Heading>
-            <Text className="text-center text-gray-600 ">
+        <Body className="bg-blue-500">
+          <Container className="my-16 flex min-w-[400px] flex-col items-center rounded-md  border bg-white p-6 text-center shadow-md ">
+            <Heading>Digital Hippo</Heading>
+            <Text className="text-gray-500 ">
               Hey {userName} let&apos;s verify your email
             </Text>
             <Button
               href={verificationLink}
-              className="mx-auto my-6 cursor-pointer rounded-md bg-blue-500 p-3 text-white"
+              className="my-6 cursor-pointer rounded-md bg-blue-500 p-3 text-white"
             >
-              Confirm Email
+              Verify Email
             </Button>
-            <Text className="text-center text-gray-600">
-              Email sent from resend for auth.
+            <Text className=" text-gray-500">
+              Email sent from digital hippo for email verification.
             </Text>
           </Container>
         </Body>
